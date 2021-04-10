aad95b1f4fb07cddaec33c5c3aadd1aa8687d864
Facundo Olano <facundo.olano@gmail.com>
2021-04-10 20:43:22 -0300

    more hygiene for python

--- for_in.py
+++ for_in.py
@@ -27,17 +27,28 @@ sections = [
     }
 ]
 
-section_position = 1
-lesson_position = 1
-for section in sections:
-    section["position"] = section_position
-    section_position += 1
 
-    if section["reset_lesson_position"]:
-        lesson_position = 1
+def traverse(sections):
+    result = []
+    section_position = 1
+    lesson_position = 1
+    for section in sections:
+        section = {**section, "position": section_position}
+        section_position += 1
 
-    for lesson in section["lessons"]:
-        lesson["position"] = lesson_position
-        lesson_position += 1
+        if section["reset_lesson_position"]:
+            lesson_position = 1
 
-print(sections)
+        lessons = []
+        for lesson in section["lessons"]:
+            lesson = {**lesson, "position": lesson_position}
+            lesson_position += 1
+            lessons.append(lesson)
+
+        section[lessons] = lessons
+        result.append(section)
+
+    return result
+
+
+print(traverse(sections))
