557688bf1285e8fd2fc4ff8d8a9d5f311bea34e0
Facundo Olano <facundo.olano@gmail.com>
2021-04-10 14:09:25 -0300

    inline fold function

--- fold.erl
+++ fold.erl
@@ -32,30 +32,31 @@
 
 traverse_sections(Sections) ->
     {Output, _, _} =
-        lists:foldl(fun traverse_section/2, {[], 1, 1}, Sections),
+        lists:foldl(
+          fun (Section, {Output, SectionPosition, LessonPosition}) ->
+                  #{lessons := Lessons, reset_lesson_position := ResetPosition} = Section,
+                  {LessonsWithPostions, NextLessonPosition} =
+                      traverse_lessons(Lessons, LessonPosition, ResetPosition),
+                  SectionWithPositions = Section#{position => SectionPosition,
+                                                  lessons => LessonsWithPostions},
+                  {[SectionWithPositions | Output], SectionPosition + 1, NextLessonPosition}
+          end, {[], 1, 1}, Sections),
     lists:reverse(Output).
-
-traverse_section(Section, {Output, SectionPosition, LessonPosition}) ->
-    #{lessons := Lessons, reset_lesson_position := ResetPosition} = Section,
-    {LessonsWithPostions, NextLessonPosition} =
-        traverse_lessons(Lessons, LessonPosition, ResetPosition),
-    SectionWithPositions = Section#{position => SectionPosition,
-                                    lessons => LessonsWithPostions},
-    {[SectionWithPositions | Output], SectionPosition + 1, NextLessonPosition}.
 
 traverse_lessons(Lessons, LessonPosition, _Reset=false) ->
     traverse_lessons(Lessons, LessonPosition);
 traverse_lessons(Lessons, _LessonPosition, _Reset=true) ->
     traverse_lessons(Lessons, 1).
 
-traverse_lessons(Lessons, LessonPosition) ->
+traverse_lessons(Lessons, FirstLessonPosition) ->
     {Output, LastLessonPosition} =
-        lists:foldl(fun traverse_lesson/2, {[], LessonPosition}, Lessons),
+        lists:foldl(
+          fun(Lesson, {Output, LessonPosition}) ->
+                  LessonWithPosition = Lesson#{position => LessonPosition},
+                  {[LessonWithPosition | Output], LessonPosition + 1}
+          end,
+          {[], FirstLessonPosition}, Lessons),
     {lists:reverse(Output), LastLessonPosition}.
-
-traverse_lesson(Lesson, {Output, LessonPosition}) ->
-    LessonWithPosition = Lesson#{position => LessonPosition},
-    {[LessonWithPosition | Output], LessonPosition + 1}.
 
 main(_) ->
     Result = traverse_sections(?SECTIONS),
